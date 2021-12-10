1d3dd2a4844bdadd9bdb11020455a4d412739790
Ediflex <94779490+Ediflex@users.noreply.github.com>
2021-12-10 20:21:21 +0530

    Update pm_filter.py

--- plugins/pm_filter.py
+++ plugins/pm_filter.py
@@ -224,7 +224,7 @@ async def cb_handler(client: Client, query: CallbackQuery):
             if (st.status == "creator") or (str(userid) in ADMINS):
                 await query.message.delete()
                 try:
-                    await query.message.reply_to_message.delete()
+                    await query.message.reply_to_message.()
                 except:
                     pass
             else:
